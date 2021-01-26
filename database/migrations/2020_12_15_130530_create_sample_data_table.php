6f2c1922120fd4720cf9bf6eb30a51998902e50f
Sam Harwig <42715585+25530@users.noreply.github.com>
2021-01-26 13:05:55 +0100

    database and public

---
+++ database/migrations/2020_12_15_130530_create_sample_data_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSampleDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sample-data', function (Blueprint $table) {
+            $table->id('id');
+            $table->string('title');
+            $table->string('description');
+            $table->string('video');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sample-data');
+    }
+}
